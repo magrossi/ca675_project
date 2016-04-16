cd84d6284502295d11b7773ee3441c21ee66e134
Anthony <lightweightdevelopment@gmail.com>
2016-04-16 20:28:00 +0100

    Refactored faces.js

--- www/static/js/faces.js
+++ www/static/js/faces.js
@@ -1,41 +1,54 @@
 (function ($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // crop face
+  /**
+   * Refreshes the current window.
+   */
+  var _refreshPage = function() {
+    window.location = window.location;
+  };
 
-  $('.fileinput-new input[name="image"]').on('change.bs.fileinput', function (e) {
+    /**
+     * Render the face bbox for the given coordinates
+     * @param {Object[]} coord - Coordinate object to render on upload panel.
+     */
+    var _renderBbox = function(coord) {
+      var coords = [coord.x, coord.y, coord.x2, coord.y2];
+      $("input[name='face_bbox']").val(coords.join());
+    };
 
-    var checkExist = setInterval(function () {
-      var img = $('.fileinput-preview img');
-      if (img.length) {
-        AddCrop(img);
-        clearInterval(checkExist);
-      }
-    }, 100);
+  /**
+   * Crop the image file bound to a given img element
+   * @param {Object[]} img - Img HTML element.
+   */
+  var _addCrop = function(img) {
+    var h = img.height();
+    var w = img.width();
 
-  });
-
-  function AddCrop(img) {
-    var h = img.height(),
-        w = img.width();
     img.Jcrop({
-      onChange: showCoords,
-      onSelect: showCoords,
+      onChange: _renderBbox,
+      onSelect: _renderBbox,
       bgFade: true,
-      bgOpacity: .3,
+      bgOpacity: 0.3,
       setSelect: [w/2-w*0.2, h/2+h*0.2, w/2+w*0.2, h/2-h*0.3]
     });
-  }
+  };
 
-  function showCoords(c) {
-    var coords = [c.x, c.y, c.x2, c.y2];
-    $("input[name='face_bbox']").val(coords.join());
-  }
+  /**
+   * Handler for an uploader element for fixed cropping.
+   * @param {Object[]} e - Respective fired fileInput event.
+   */
+  var _onUploadAddCrop = function(e) {
+    var intervalId = setInterval(function () {
+      var img = $('.fileinput-preview img');
+      if (img.length) {
+        _addCrop(img);
+        clearInterval(intervalId);
+      }
+    }, 100);
+  };
 
-  // refresh if pending
-
-  $('.refresh-page').on('click', function () {
-    window.location = window.location;
-  });
-
-})(jQuery); // End of use strict
+  // bind refresh and on upload actions
+  $('.refresh-page').on('click', _refreshPage);
+  $('.fileinput-new input[name="image"]').on('change.bs.fileinput', _onUploadAddCrop);
+})(jQuery);
